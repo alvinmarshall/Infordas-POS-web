72be28add2feb9a52b3a9570c5af7ff2c1fb6cc9
bik_krlvn <kelvinbirikorang@gmail.com>
2019-12-22 09:17:19 +0000

    Updates

---
+++ precache-manifest.18df17523d8a39a4de6c802a2977ac97.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83bd4614ff8d0379b1015f6ee1d37328",
+    "url": "/infordas-pos/index.html"
+  },
+  {
+    "revision": "3b8f4750be3144e7926b",
+    "url": "/infordas-pos/static/js/2.ca8a1760.chunk.js"
+  },
+  {
+    "revision": "5e92cdaebcf395a6362f",
+    "url": "/infordas-pos/static/js/main.60a58f83.chunk.js"
+  },
+  {
+    "revision": "3bcd128ba4ecfeac1731",
+    "url": "/infordas-pos/static/js/runtime-main.0b1f9c65.js"
+  }
+]);
