26eee34bc7fe491be83b16f1db46da5850b80e86
bik_krlvn <kelvinbirikorang@gmail.com>
2019-12-23 14:26:59 +0000

    fixed heroku proxy issues

--- src/components/Auth/Login/setAuthToken.js
+++ src/components/Auth/Login/setAuthToken.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const setAuthToken = token => {
+  axios.defaults.baseURL = (process.env.NODE_ENV !== 'production') ? 'http://localhost:9000' : 'https://protected-reef-45264.herokuapp.com'
   if (token) {
     axios.defaults.headers.common["Authorization"] = token;
     return;
