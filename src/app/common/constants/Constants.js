9a0dfc7b74a0900358a06d123c935ad26da7d32d
bik_krlvn <kelvinbirikorang@gmail.com>
2019-12-23 15:42:59 +0000

    commit config var heroku

--- src/app/common/constants/Constants.js
+++ src/app/common/constants/Constants.js
@@ -30,9 +30,7 @@ export const statusOption = [
   { key: "1", value: "Active" }
 ];
 
-export const BASE_URL = process.env.PORT
-  ? "https://protected-reef-45264.herokuapp.com"
-  : "http://localhost:9000";
+export const BASE_URL = process.env.BASE_URL || "http://localhost:9000";
 export const downloadUrl = `${BASE_URL}/files/download?url`;
 
 export const ALERT_MODAL = "AlertModal";
